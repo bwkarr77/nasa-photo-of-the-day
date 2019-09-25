915eecc6ed4f53aca8c0db8b4aabcd321607e76c
Brett Karr <b.karr@polyclip.us>
2019-09-24 22:46:48 -0500

    commit for the night

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,8 @@ import React from "react";
 const Footer = props => {
   return (
     <div className="footer">
-      <h1>Footer Section</h1>
       <p>{props.copyright}</p>
-      <p>{props.service_version}</p>
+      <p>Version: {props.service_version}</p>
     </div>
   );
 };
